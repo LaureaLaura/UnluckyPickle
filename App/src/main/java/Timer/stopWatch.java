70fcb40aa674d47f948b6bf8415fe8491da93b1d
Elisa Stufano <127190820+LaureaLaura@users.noreply.github.com>
2023-11-08 17:03:15 +0100

    Update stopWatch.java

--- App/src/main/java/Timer/stopWatch.java
+++ App/src/main/java/Timer/stopWatch.java
@@ -1,29 +1,41 @@
 package Timer;
-
+ 
 public class stopWatch extends Thread {
     static long startTime;
     static double elapsedTime;
-
+ 
     public void run() {
         startTime = System.nanoTime();
     }
-
+ 
     public void getTimeStopGame() {
         elapsedTime = (System.nanoTime() - startTime) / 1000000000;
         if (elapsedTime < 60) {
-            System.out.printf("SEI RIUSCUTAO A FINIRE IL GIOCO IN %.1f SECONDI\n", elapsedTime);
+            System.out.printf("SEI RIUSCUTO A FINIRE IL GIOCO IN %.1f SECONDI!\n", elapsedTime);
         } else {
-            System.out.printf("SEI RIUSCITO A FINIRE IL GIOCO IN %.1f SECONDI\n", elapsedTime / 60);
+            int sec;
+            if(elapsedTime%60 == 0){
+            System.out.printf("HAI GIOCATO PER %.1f MINUTI!\n", elapsedTime / 60);
+            } else {
+                sec = (int) elapsedTime%60;
+                System.out.printf("HAI GIOCATO PER %d MINUTI E %d SECONDI!\n", (int) elapsedTime / 60, sec);
+            }
         }
     }
-
+ 
     public void getTimeEndGame() {
         elapsedTime = (System.nanoTime() - startTime) / 1000000000;
         if (elapsedTime < 60) {
-            System.out.printf("HAI GIOCATO PER %.1f SECONDI\n", elapsedTime);
+            System.out.printf("HAI GIOCATO PER %.1f SECONDI!\n", elapsedTime);
         } else {
-            System.out.printf("HAI GIOCATO PER %.1f MINUTI\n", elapsedTime / 60);
+            int sec;
+            if(elapsedTime%60 == 0){
+            System.out.printf("HAI GIOCATO PER %.1f MINUTI!\n", elapsedTime / 60);
+            } else {
+                sec = (int) elapsedTime%60;
+                System.out.printf("HAI GIOCATO PER %d MINUTI E %d SECONDI!\n", (int) elapsedTime / 60, sec);
+            }
         }
-
+ 
     }
 }
